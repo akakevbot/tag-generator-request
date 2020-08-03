a7932944d58afbc06a2667568b2de1a5f9e5954b
akakevbot <40043305+akakevbot@users.noreply.github.com>
2020-08-03 11:33:12 -0700

    Add files via upload

--- request.js
+++ request.js
@@ -2,8 +2,13 @@ const express = require('express');
 const https = require('https');
 const app = express();
 const port = 3000;
-app.use(allowCrossDomain)
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.get('/', (req, res) => { 
     res.send("This is the home page");    
