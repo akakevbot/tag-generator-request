fcdc9c91f79e6942e491226ccf16a116041fa087
akakevbot <40043305+akakevbot@users.noreply.github.com>
2020-08-03 11:37:30 -0700

    Add files via upload

--- request.js
+++ request.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const https = require('https');
+const cors = require('cors');
 const app = express();
 const port = 3000;
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+
+app.use(cors());
+app.options('*', cors()); // this will enable cors for all your route.
 
 app.get('/', (req, res) => { 
     res.send("This is the home page");    
